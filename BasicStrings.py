07ed8c13964efa757ebb78c6fabc2823b33ae89c
“Dominique <the.dominique.nicole@outlook.com>
2022-11-20 10:40:56 -0600

    My first commit

---
+++ BasicStrings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#we are working on strings and numbers aka str and int
+#make the informaion a comment so when I see a pound sign it meqans its a code comment or non runable information
+
+
+#variables are A B X for this exercise please align values to the variables
+A = 55 # A is a type of int (int is short for interger) 
+B = "Tony Montana" #B is a type of str (str is short for string)
+X = "2300 Jackson Street" # X is a type of str
+
+print(X)
+print(A)
+print(B)
+
+# Great Sunday 11/20/22
